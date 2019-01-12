693069f5a242bfce3cc903119ddfca52afe94cd1
yash98 <yashmalviya98@gmail.com>
2019-01-12 09:39:00 +0530

    correct minor syntax error in Marix.h

--- src/Matrix/Matrix.h
+++ src/Matrix/Matrix.h
@@ -9,13 +9,14 @@ public:
     // DATA MEMBERS
     std::vector<std::vector<float>> matrix;
 
+    // OPTIONS FOR FUNCTIONS
     enum convolMethod {simpleConvol, matrixMult};
     enum nonLinearActMethod {relu, tanH, softmax, sigmoid};
     enum poolingMethod {maxPooling, avgPooling};
 
     // CONSTRUCTORS
     Matrix(std::vector<std::vector<float>> inputMatrix);
-    Matrix(string fileName):
+    Matrix(std::string fileName);
 
     // FUNCTION MEMBERS
     Matrix convolution(const Matrix& kernel, bool doPadding, convolMethod method);
@@ -23,7 +24,7 @@ public:
     Matrix nonLinearActivation(nonLinearActMethod method, bool returnInSame);
 
     Matrix pooling(poolingMethod method, bool returnInSame);
-}
+};
 
 
 #endif  /* MATRIX_H */
